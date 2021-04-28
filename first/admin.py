7e496748be55da01bcbf463f099fcb9f79135c5d
Rafik <fantar.rafik@gmail.com>
2021-04-28 13:56:17 +0200

    update models

--- first/admin.py
+++ first/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Songs
 from .models import MyFile
+from .models import Category
+from .models import Terme
+from .models import Profil
+from .models import ProfilCategoryScore
 
 # Register your models here.
 admin.site.register(Songs)
 admin.site.register(MyFile)
+admin.site.register(Category)
+admin.site.register(Terme)
+admin.site.register(Profil)
+admin.site.register(ProfilCategoryScore)
--- first/models.py
+++ first/models.py
@@ -15,3 +15,32 @@ class MyFile(models.Model):
     resume = models.FileField(blank=False, null=False)
 
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+    name =  models.CharField(max_length=255,null=False)
+
+class Terme(models.Model):
+    title =  models.CharField(max_length=255,null=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Profil(models.Model):
+    firstname = models.CharField(max_length=255, null=False)
+    lastname = models.CharField(max_length=255)
+    dateOfBirth = models.DateField
+    yearsOfExperience = models.DecimalField(blank=True, null=True, max_digits=2,  decimal_places=1,default=1.0)
+    profilLabel = models.CharField(max_length=255)
+    attachment = models.ForeignKey(MyFile,on_delete=models.CASCADE)
+
+
+class ProfilCategoryScore(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    profil = models.ForeignKey(Profil,on_delete=models.CASCADE)
+    score = models.DecimalField(blank=True, null=True, max_digits=4,  decimal_places=2,default=1.0)
+
+
+
+
+
+
+
+
